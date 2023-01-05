91901e971e3b6bed5684b3b9cd11ccf49f4f6b5b
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2023-01-05 23:31:21 +0700

    feat: section bunner, carousel.js, imges for carousel

---
+++ modules/carousel.js
@@ -0,0 +1,23 @@
+let bunner = document.querySelector('.bunner-carousel');
+let bunnerUnder = document.querySelector('.bunner-carousel-under');
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+
+
+let i = 1;
+arrowLeft.addEventListener('click', function(){
+  i++;  
+  if(i>7){
+    i=1;
+  }
+  bunner.style.background = `linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(46, 46, 46, 0.5)),url("../../assets/images/bunner/bunner-${i}.jpg")`;
+
+});
+
+arrowRight.addEventListener('click', function(){
+  i--;  
+  if(i<1){
+    i=7;
+  }
+  bunner.style.background = `linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(46, 46, 46, 0.5)),url("../../assets/images/bunner/bunner-${i}.jpg")`;
+});
