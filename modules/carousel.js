9a4ab962e8f7cc943358b44307dd8026ac816b7b
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2023-02-15 13:07:47 +0700

    feat: added form

--- modules/carousel.js
+++ modules/carousel.js
@@ -5,22 +5,21 @@ let arrowRight = document.querySelector('.arrow-right');
 
 
 let i = 1;
-arrowLeft.addEventListener('click', function(){
-  i++;  
-  if(i>7){
-    i=1;
+arrowLeft.addEventListener('click', function () {
+  i++;
+  if (i > 7) {
+    i = 1;
   }
   bunner.style.background = `linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(46, 46, 46, 0.5)),url("./assets/images/bunner/bunner-${i}.jpg")`;
   bunner.style.backgroundSize = "cover";
 
 });
 
-arrowRight.addEventListener('click', function(){
-  i--;  
-  if(i<1){
-    i=7;
+arrowRight.addEventListener('click', function () {
+  i--;
+  if (i < 1) {
+    i = 7;
   }
   bunner.style.background = `linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(46, 46, 46, 0.5)),url("./assets/images/bunner/bunner-${i}.jpg")`;
   bunner.style.backgroundSize = "cover";
 });
-
